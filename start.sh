6f2aa9a58c8be77ac7c4b60fedd1bad5ff150543
cheruden <cheruden25@gmail.com>
2021-06-11 22:33:07 +0300

    add start.sh file

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY='ae1fb9b194c088f4308f4b273ff43101'
+export SECRET_KEY='12345'
+export MAIL_USERNAME='cheruden25@gmail.com'
+export MAIL_PASSWORD='0723056032Aa'
+
+python3.8 manage.py server
